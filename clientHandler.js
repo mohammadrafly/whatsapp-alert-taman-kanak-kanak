82a839ed0cec877d5e7f59b9ce6e2097fc3d79fe
Mohammad Rafly <mohammadrafly19@gmail.com>
2024-05-19 13:03:55 +0700

    changing web version

--- clientHandler.js
+++ clientHandler.js
@@ -1,11 +1,14 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
+const wwebVersion = '2.2412.54';
 
 const client = new Client({
     authStrategy: new LocalAuth(),
-    puppeteer: {
-        args: ['--no-sandbox'],
-    }
+    puppeteer: {},
+    webVersionCache: {
+        type: 'remote',
+        remotePath: `https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/${wwebVersion}.html`,
+    },
 });
 
 async function generateQRCode(data) {
