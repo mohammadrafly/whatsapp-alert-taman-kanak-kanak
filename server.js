aab7b2703817377b557dc4ed249d20c3d0d69f4f
Mohammad Rafly <mohammadrafly19@gmail.com>
2024-05-21 19:46:11 +0700

    update error handling and cron jobs

--- server.js
+++ server.js
@@ -2,13 +2,22 @@ const { client } = require('./utils/clientHandler');
 const qrcode = require('qrcode-terminal');
 const { setupCronJob } = require('./utils/cronJob');
 
+client.on('qr', qr => {
+    console.log('QR code received');
+    qrcode.generate(qr, { small: true });
+});
+
 client.on('ready', () => {
     console.log('Client is ready!');
+    setupCronJob();
 });
 
-client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
+client.on('error', (err) => {
+    console.error('Client error:', err);
 });
 
-client.initialize();
-setupCronJob(); 
+try {
+    client.initialize();
+} catch (error) {
+    console.error('Initialization error:', error);
+}
